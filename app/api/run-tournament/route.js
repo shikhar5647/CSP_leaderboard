bbefc61fbec085c8fcdd5143dfe4468484570ac6
Shikhar Dave <b22ch032@iitj.ac.in>
2025-10-11 10:22:24 +0530

    run_tor

---
+++ app/api/run-tournament/route.js
@@ -0,0 +1,6 @@
+import { NextResponse } from 'next/server';
+import { writeFile, mkdir } from 'fs/promises';
+import { existsSync } from 'fs';
+import path from 'path';
+import { runTournament } from '@/lib/tournament-runner';
+import { addSubmission } from '@/lib/db';
